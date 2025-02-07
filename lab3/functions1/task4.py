54f272ab68d6934866c7cadac3f8000bd2496087
heujinn <166709021+heujinn@users.noreply.github.com>
2025-02-07 07:50:18 +0500

    upd

---
+++ lab3/functions1/task4.py
@@ -0,0 +1,16 @@
+def isPrime(x):
+    cnt = 0
+    for i in range(1, x + 1):
+        if x % i == 0:
+            cnt = cnt + 1
+    if cnt == 2:
+        return True
+    return False
+
+def filter_prime(nums):
+    primes = [x for x in nums if isPrime(x)]
+    return primes
+
+inp1 = input().split()
+inp2 = map(int, inp1)
+print(filter_prime(inp2))
